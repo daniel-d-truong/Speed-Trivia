6811f37e31a43f0da98b76b62e05a70e5b6f3582
Daniel Truong <ddtruong06@gmail.com>
2018-08-24 13:18:33 -0700

    Created question and multiple choice radio buttons

---
+++ app/src/main/java/com/example/danie/testappusingjava/Trivia.java
@@ -0,0 +1,13 @@
+package com.example.danie.testappusingjava;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Trivia extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trivia);
+    }
+}
