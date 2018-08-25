30411add14897af3e37aa01c2fa6a2292cf82264
Daniel Truong <ddtruong06@gmail.com>
2018-08-24 20:05:45 -0700

    Changes question when button is clicked

--- app/src/main/java/com/example/danie/testappusingjava/Trivia.java
+++ app/src/main/java/com/example/danie/testappusingjava/Trivia.java
@@ -2,12 +2,47 @@ package com.example.danie.testappusingjava;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import javax.xml.namespace.QName;
+
+
 
 public class Trivia extends Activity {
+
+    private Button btn;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia);
+
+        btn = (Button) findViewById(R.id.submit);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                changeQuestion();
+                changeChoices();
+            }
+        });
     }
+
+    private void changeQuestion(){
+        count++;
+
+        TextView question = (TextView) findViewById(R.id.question);
+        String message = getString(R.string.question);
+        message+=" "+count;
+
+        question.setText(message);
+    }
+
+    private void changeChoices() {
+        System.out.println(0);
+    }
+
+
 }
