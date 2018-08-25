12598661dba24d07a85450a0530ef53a1429c9c3
Daniel Truong <ddtruong06@gmail.com>
2018-08-24 21:44:41 -0700

    Failed attempt to create snackbar

--- app/src/main/java/com/example/danie/testappusingjava/Trivia.java
+++ app/src/main/java/com/example/danie/testappusingjava/Trivia.java
@@ -2,13 +2,16 @@ package com.example.danie.testappusingjava;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.util.Log;
 
 import javax.xml.namespace.QName;
 
+import static android.support.constraint.Constraints.TAG;
 
 
 public class Trivia extends Activity {
@@ -22,16 +25,21 @@ public class Trivia extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia);
 
-        choices = (RadioGroup) findViewById (R.id.multiple_choice);
+        choices = (RadioGroup) findViewById (R.id.multiple_choice); //must be given a value AFTER the xml has been loaded
         btn = (Button) findViewById(R.id.submit);
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (choices.getCheckedRadioButtonId() != -1) {
+                if (choices.getCheckedRadioButtonId() != -1) { //checks if user has picked a choice
                     changeQuestion();
                     changeChoices();
                     resetRadio();
                 }
+//                else { //creates pop-up telling user to make a choice
+//                    Log.d(TAG, "Else Statement has been Run.");
+//                    Snackbar.make(findViewById(R.id.myLayout), R.string.make_choice, Snackbar.LENGTH_SHORT)
+//                            .show();
+//                }
             }
         });
     }
