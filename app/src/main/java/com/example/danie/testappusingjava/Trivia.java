626b207458d58b93d70a91a713ed85683987463f
Daniel Truong <ddtruong06@gmail.com>
2018-08-24 18:01:32 -0700

    Revert "Attempt for file to read JSON and use trivia API"
    This reverts commit a8e877b9feb6aa67e4d39f4a2e7d4ef084b734f0.

--- app/src/main/java/com/example/danie/testappusingjava/Trivia.java
+++ app/src/main/java/com/example/danie/testappusingjava/Trivia.java
@@ -2,23 +2,12 @@ package com.example.danie.testappusingjava;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import JSON.*;
-
-import static android.support.constraint.Constraints.TAG;
-import static java.lang.System.out;
 
 public class Trivia extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "Trivia Activity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia);
-//
-//        MainParser json = new MainParser();
-//        Log.d(TAG, json.getJSON());
     }
-
-
 }
