ce1c1521851c6312c7b4eb2be8637fdeb8b47b2a
Daniel Truong <ddtruong06@gmail.com>
2018-09-21 00:24:14 -0700

    Created selectr_bottombar_text.xml and made bottomnavigation bg white and unactive icons to be greyd out

--- app/src/main/java/com/example/danie/testappusingjava/MainActivity.java
+++ app/src/main/java/com/example/danie/testappusingjava/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.danie.testappusingjava;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -33,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         //CREATES bottom navigation view
         toolbar = getSupportActionBar();
         BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.navigationView);
+        bottomNav.setBackgroundColor(Color.WHITE);
+
         final BlankFragment homeFragment = new BlankFragment();
         final QuizFragment triviaFragment = new QuizFragment();
         final AccountFragment settingsFragment = new AccountFragment();
@@ -69,6 +73,10 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    private int fetchColor(int color){
+        return ContextCompat.getColor(this, color);
     }
 
 
